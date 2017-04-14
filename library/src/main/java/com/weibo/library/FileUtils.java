be55eb858c84bc2de05a6ea97ac6542ff6f035b9
weibo <gt1254094162@163.com>
2017-04-14 21:32:27 +0800

    20170414第一次提交

---
+++ library/src/main/java/com/weibo/library/FileUtils.java
@@ -0,0 +1,73 @@
+package com.weibo.library;
+
+import com.weibo.volley.client.App;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by weibo on 17-4-14.
+ */
+
+public class FileUtils {
+
+  /**
+   * 获取文件夹对象
+   *
+   * @return 返回SD卡下的指定文件夹对象，若文件夹不存在则创建
+   */
+  public static File getExternalCacheDir(String folderName) {
+    File file = new File(App.INSTANCE.getCacheDir()
+        + File.separator + folderName + File.separator);
+    file.mkdirs();
+    return file;
+  }
+
+  /**
+   * 输入流转byte[]
+   */
+  public static byte[] input2byte(InputStream inStream) {
+    if (inStream == null) {
+      return null;
+    }
+    byte[] in2b = null;
+    BufferedInputStream in = new BufferedInputStream(inStream);
+    ByteArrayOutputStream swapStream = new ByteArrayOutputStream();
+    int rc = 0;
+    try {
+      while ((rc = in.read()) != -1) {
+        swapStream.write(rc);
+      }
+      in2b = swapStream.toByteArray();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      closeIO(inStream, in, swapStream);
+    }
+    return in2b;
+
+  }
+
+  /**
+   * 关闭流
+   */
+  public static void closeIO(Closeable... closeables) {
+    if (null == closeables || closeables.length <= 0) {
+      return;
+    }
+    for (Closeable cb : closeables) {
+      try {
+        if (null == cb) {
+          continue;
+        }
+        cb.close();
+      } catch (IOException e) {
+        throw new RuntimeException(
+            FileUtils.class.getClass().getName(), e);
+      }
+    }
+  }
+}
---
+++ library/src/main/java/com/weibo/library/client/App.java
@@ -0,0 +1,29 @@
+package com.weibo.library.client;
+
+import android.app.Application;
+
+/**
+ * Created by weibo on 17-4-14.
+ */
+
+public class App {
+
+  public static final Application INSTANCE;
+
+  static {
+    Application app = null;
+    try {
+      app = (Application) Class.forName("android.app.AppGlobals").getMethod("getInitialApplication").invoke(null);
+      if (app == null)
+        throw new IllegalStateException("Static initialization of Applications must be on main thread.");
+    } catch (final Exception e) {
+      try {
+        app = (Application) Class.forName("android.app.ActivityThread").getMethod("currentApplication").invoke(null);
+      } catch (final Exception ex) {
+        ex.printStackTrace();
+      }
+    } finally {
+      INSTANCE = app;
+    }
+  }
+}
