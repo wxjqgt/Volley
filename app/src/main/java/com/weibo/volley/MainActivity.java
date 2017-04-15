09785976ee3b156d75114c3fb155e72a717951bd
weibo <gt1254094162@163.com>
2017-04-15 22:39:44 +0800

    20170414第6次提交

--- app/src/main/java/com/weibo/volley/MainActivity.java
+++ app/src/main/java/com/weibo/volley/MainActivity.java
@@ -6,30 +6,73 @@ import com.google.gson.Gson;
 import com.weibo.library.Volley;
 import com.weibo.library.client.HttpCallback;
 import com.weibo.library.client.HttpParams;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.internal.operators.observable.ObservableFromIterable;
+import java.util.Map;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PartMap;
 
-public class MainActivity extends AppCompatActivity {
+import static com.weibo.volley.FaxianApi.url;
 
-  String url = "http://api.meishi.cc/v6/faxian_new2.php?format=json";
+public class MainActivity extends AppCompatActivity implements HttpCallback.SuccessWithString {
+
   //source=android&lon=&lat=&format=json&
   Gson gson = new Gson();
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    /*Retrofit retrofit =
+        new Retrofit.Builder().addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(url)
+            .build();
+    Map<String, String> params1 = new HashMap<>();
+    params1.put("source", "android");
+    params1.put("lon", "");
+    params1.put("lat", "");
+    params1.put("format", "json");
+
+    final long c1 = System.currentTimeMillis();
+    retrofit.create(FaxainApi.class)
+        .getData(params1)
+        .subscribeOn(Schedulers.io())
+        .subscribe(new Consumer<Faxian>() {
+          @Override public void accept(Faxian faxian) throws Exception {
+            System.out.println("faxainR = " + faxian.toString());
+            System.out.println("timeR = " + (System.currentTimeMillis() - c1));
+          }
+        });*/
+
     HttpParams params = new HttpParams();
     params.put("source", "android");
     params.put("lon", "");
     params.put("lat", "");
     params.put("format", "json");
-    final long c = System.currentTimeMillis();
     Volley.post()
-        .url(url)
+        .url(url + "v6/faxian_new2.php?format=json")
         .params(params)
-        .onSuccessWithString(new HttpCallback.SuccessWithString() {
-            @Override public void onSuccess(String t) {
-              System.out.println("t = " + t);
-            }
-        })
+        .onSuccessWithString(this)
         .doTask();
   }
+
+  @Override public void onSuccess(String t) {
+    final Faxian faxian = gson.fromJson(t, Faxian.class);
+    ObservableFromIterable.fromIterable(faxian.getFaxian_list())
+        .forEach(new Consumer<Faxian.FaxianListBean>() {
+          @Override public void accept(Faxian.FaxianListBean faxianListBean) throws Exception {
+            System.out.println(faxianListBean.getTitle());
+          }
+        });
+  }
+}
+
+interface FaxianApi {
+  String url = "http://api.meishi.cc/";
+
+  @POST("v6/faxian_new2.php?format=json") @Multipart Observable<Faxian> getData(
+      @PartMap Map<String, String> params);
 }
